8d38fb0f99ace3a848f739d50d9ff069b4409432
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-09-20 17:36:57 -0400

    Update main (19).cpp

--- main (19).cpp
+++ main (19).cpp
@@ -12,57 +12,42 @@ main ()
   int y;
  
 int cts;
-  
-int ct;
-  
-int sts;
-  
-int st;
-  
-int lnt;
-  
-int cgc;
-  
- 
+int ct;  
+int sts;  
+int dlt;  
+int lnt;  
+int cgc;   
 cout << "Welcome to CPP Taco Bell!";
-    cout <<"Enter 1 for Crunchy Taco Supreme, 2 for Soft Taco Supreme, 3 for Doritos Locos Tacos, 4 for Chalupa Supreme";
+    cout <<"Enter 1 for Crunchy Taco Supreme, 2 for Soft Taco Supreme, 3 for Doritos Locos Tacos Supreme, 4 for Chalupa Supreme";
 cin >> x;
-  
- 
-if (x == 1)
-    {
-
+   
+if (x == 1){
 cout << "Please enter the amount tacos:";
 cin >> y;
-
 cts = (1.89*y)*1.06;
       
 cout << "Your total will be " << cts;
-cout << "  Thank You for visiting CPP Dominoes." ;
+cout << "  Thank You for visiting CPP Taco Bell." ;
 return 0;
     
-} else if(x == 2){
+}else if(x == 2){
 
-cout << "Please enter the size(1 for small, 2 for medium, 3 for large):";
+cout << "Please enter the amount of Soft Tacos:):";
 cin >> y;
-
-if(y==1){
-    ct=6;
-}else if(y==2){
-    ct=8;
-}else if(y==3){
-    ct=10;
-}
-      
+sts = (1.89*y)*1.06;
+ 
 cout << "Your total will be " << ct;
-cout << "  Thank You for visiting CPP Dominoes." ;
+cout << "  Thank You for visiting CPP Taco Bell." ;
 return 0;
-    
-}
 
+}else if(x == 3){
+
+cout << "Please enter the amount of Doritos Locos Tacos Supreme:):";
+cin >> y;
+dlt = (2.39*y)*1.06;
  
- 
- 
- 
- 
+cout << "Your total will be " << ct;
+cout << "  Thank You for visiting CPP Taco Bell." ;
+return 0;
+
 }
